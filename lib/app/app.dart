cb107473f83237427a6957567a9f1fdf3f391bbf
hossain-mo <hossainfcih@gmail.com>
2022-10-12 16:45:27 +0200

    add new line

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,6 +4,7 @@ import '../presentation/resources/theme_manager.dart';
 // test push to hussain
 // main
 // test push to asmaa
+// test push to jjj
 class MyApp extends StatefulWidget {
  // const MyApp({Key? key}) : super(key: key);
   MyApp._internal();
